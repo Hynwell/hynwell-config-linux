9e0b30488f5ad7d3f006b8aaf18e2f847ef6e924
Hynwell <hynwellG@gmail.com>
2023-02-22 17:43:11 +0300

    update startup.sh

--- .config/startup.sh
+++ .config/startup.sh
@@ -6,16 +6,16 @@
 #echo "${USERNAME}   ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers.d/myconfig
 
 #user
-#echo "---Linux UPDATE---"
+echo "---Linux UPDATE---"
 sudo apt update && sudo apt upgrade -y
 
-#echo "---Install default software---"
-for software_list in git zsh lsd bat dust duf procs dog fd gtop curlie micro screen tldr curl 
+echo "---Install SOFT---"
+for software_list in git zsh bat gtop micro screen tldr curl 
 	do
 		sudo apt install $software_list -y
 	done
 
-#echo "---Install ZSH---"
+echo "---Install ZSH---"
 sudo apt install zsh -y
 
 mkdir -p ~/.config/zsh/plugins/
@@ -24,11 +24,13 @@ curl https://raw.githubusercontent.com/Hynwell/hynwell-config-linux/main/.config
 curl https://raw.githubusercontent.com/Hynwell/hynwell-config-linux/main/.config/zsh/.alias > ~/.config/zsh/.alias
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/powerlevel10k
-git clone git@github.com:marlonrichert/zsh-autocomplete.git ~/.config/zsh/plugins/zsh-autocomplete
-git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete.git ~/.config/zsh/plugins/zsh-autocomplete
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
+
+echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
 chsh --shell /bin/zsh $USER
 
-#echo "---Install Docker---"
+echo "---Install Docker---"
 for software_list_for_docker in ca-certificates gnupg lsb-release
 	do
 		sudo apt install $software_list_for_docker -y
@@ -43,8 +45,10 @@ echo \
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+docker --version
+docker compose version 
 
-
+zsh
 
 #ssh-keygen -t rsa -b 4096 -C "github@gmail.com"
 #cat .ssh/id_rsa.pub 
