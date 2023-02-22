b7285ba2eeab5f7294f0cdfb19f9bdb8cb1548fa
Hynwell <hynwellG@gmail.com>
2023-02-22 20:08:17 +0300

    update startup.sh

--- .config/startup.sh
+++ .config/startup.sh
@@ -9,18 +9,20 @@
 echo "---Linux UPDATE---"
 sudo apt update && sudo apt upgrade -y
 
-echo "---Install SOFT---"
 
+echo "---Install SOFT---"
 softwares_list=(git zsh bat gtop micro screen tldr curl) 
 for software_list in ${softwares_list[@]}
 	do
 		sudo apt install ${software_list} -y
 	done
 
+
 echo "---Install ZSH---"
 sudo apt install zsh -y
 
 mkdir -p ~/.config/zsh/plugins/
+
 curl https://raw.githubusercontent.com/Hynwell/hynwell-config-linux/main/.zshrc > ~/.zshrc
 curl https://raw.githubusercontent.com/Hynwell/hynwell-config-linux/main/.config/zsh/.p10k.zsh > ~/.config/zsh/.p10k.zsh
 curl https://raw.githubusercontent.com/Hynwell/hynwell-config-linux/main/.config/zsh/.alias > ~/.config/zsh/.alias
@@ -34,7 +36,6 @@ chsh --shell /bin/zsh $USER
 
 
 echo "---Install Docker---"
-
 softwares_list_for_docker=(ca-certificates gnupg lsb-release)
 for software_list_for_docker in ${softwares_list_for_docker[@]}
 	do
@@ -43,16 +44,17 @@ for software_list_for_docker in ${softwares_list_for_docker[@]}
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
 docker --version
 docker compose version 
 
+# start ZSH
 zsh
 
 #ssh-keygen -t rsa -b 4096 -C "github@gmail.com"
